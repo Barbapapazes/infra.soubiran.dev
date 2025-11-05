c203b71d1a66ce320d4091a9e948e910a767e7fd
Barbapapazes <e.soubiran25@gmail.com>
2025-11-05 17:46:13 +0100

    feat: unpic

--- packages/vite/vite.config.ts
+++ packages/vite/vite.config.ts
@@ -1,9 +1,14 @@
 /// <reference types="vite-ssg" />
 import type MarkdownIt from 'markdown-it'
+import type { MarkdownExit } from 'markdown-it'
 import type { UserConfig } from 'vite'
 import ui from '@nuxt/ui/vite'
 import shiki from '@shikijs/markdown-it'
+import { getPixels } from '@unpic/pixels'
+import { blurhashToDataUri } from '@unpic/placeholder'
 import vue from '@vitejs/plugin-vue'
+import { encode } from 'blurhash'
+import { imageSize } from 'image-size'
 import MarkdownItGitHubAlerts from 'markdown-it-github-alerts'
 // @ts-expect-error No declaration file
 import implicitFigures from 'markdown-it-image-figures'
@@ -80,7 +85,7 @@ export default (title: string, hostname: string) => defineConfig({
 
         md.use(implicitFigures, { figcaption: 'alt' })
 
-        md.use(linkAttributes, [
+        md.use(linkAttributes as any, [
           {
             matcher: (link: string) => /^https?:\/\/soubiran\.dev/.test(link),
             attrs: {
@@ -110,19 +115,30 @@ export default (title: string, hostname: string) => defineConfig({
           }
         })
 
-        md.use((md: MarkdownIt) => {
+        md.use((md: MarkdownExit) => {
           const imageRule = md.renderer.rules.image!
-          md.renderer.rules.image = (tokens, idx, options, env, self) => {
+          md.renderer.rules.image = async (tokens, idx, options, env, self) => {
             const token = tokens[idx]
             const src = token.attrGet('src')
 
             if (src) {
-              const isExternal = src.startsWith('http') || src.startsWith('//')
+              const isExternal = src.startsWith('http')
 
               if (!isExternal) {
-                token.attrSet('width', '768')
-                token.attrSet('height', '400')
-                token.attrSet('src', joinURL(`https://${hostname}`, 'cdn-cgi/image', 'width=1200,quality=80,format=auto', `https://assets.${hostname}`, src))
+                const remoteSrc = joinURL(`https://${hostname}`, 'cdn-cgi/image', 'width=1200,quality=80,format=auto', `https://assets.${hostname}`, src)
+
+                const img = await fetch(remoteSrc).then(res => res.bytes())
+
+                const { width, height } = imageSize(img)
+
+                const { data } = await getPixels(img)
+                const blurhash = encode(Uint8ClampedArray.from(data), width, height, 4, 4)
+
+                token.attrSet('width', width.toString())
+                token.attrSet('height', height.toString())
+                token.attrSet('loading', 'lazy')
+                token.attrSet('style', `background-size: cover; background-image: url(${blurhashToDataUri(blurhash)});`)
+                token.attrSet('src', remoteSrc)
               }
             }
 
