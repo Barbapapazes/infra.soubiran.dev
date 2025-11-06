fdfe33f5158347f456b6bd164d30a8b85f9118c1
barbapapazes <e.soubiran25@gmail.com>
2025-11-06 12:53:53 +0100

    build: add caching for img

--- packages/vite/vite.config.ts
+++ packages/vite/vite.config.ts
@@ -1,5 +1,8 @@
 /// <reference types="vite-ssg" />
 import type { UserConfig } from 'vite'
+import { Buffer } from 'node:buffer'
+import { mkdir, readFile, writeFile } from 'node:fs/promises'
+import { dirname, join } from 'node:path'
 import ui from '@nuxt/ui/vite'
 import shiki from '@shikijs/markdown-it'
 import { getPixels } from '@unpic/pixels'
@@ -125,11 +128,17 @@ export default (title: string, hostname: string) => defineConfig({
               if (!isExternal) {
                 const remoteSrc = joinURL(`https://${hostname}`, 'cdn-cgi/image', 'width=1200,quality=80,format=auto', `https://assets.${hostname}`, src)
 
-                const img = await fetch(remoteSrc).then(res => res.bytes())
+                const file = join('.cache', src)
+                let img: Uint8Array<ArrayBufferLike> | undefined = await readFile(file).then(bin => Buffer.from(bin)).catch(() => undefined)
+                if (!img) {
+                  img = await fetch(remoteSrc).then(res => res.bytes())
+                  await mkdir(dirname(file), { recursive: true })
+                  await writeFile(file, Buffer.from(img!))
+                }
 
-                const { width, height } = imageSize(img)
+                const { width, height } = imageSize(img!)
 
-                const { data } = await getPixels(img)
+                const { data } = await getPixels(img!)
                 const blurhash = encode(Uint8ClampedArray.from(data), width, height, 4, 4)
 
                 token.attrSet('src', remoteSrc)
